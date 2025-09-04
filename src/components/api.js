357ae06dcee5d851031b2aaf2d61860de5568277
Sardorbek <rizotoha1978@gmail.com>
2025-09-04 20:14:26 +0500

    Initial commit

--- src/components/api.js
+++ src/components/api.js
@@ -1,13 +1,22 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://127.0.0.1:8000/",
+// });
+
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("access");
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
-});
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+  baseURL: "http://127.0.0.1:8000/", // yoki sening backend URL’ing
+  withCredentials: true, // agar cookie ishlatsa
 });
 
 export default API;
