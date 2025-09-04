e830b3f88b3303c8b632fb44dec2362cd97da5a6
Sardorbek <rizotoha1978@gmail.com>
2025-09-04 20:08:59 +0500

    Initial commit

--- src/components/api.js
+++ src/components/api.js
@@ -1,16 +1,16 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: "http://127.0.0.1:8000/",
-// });
+const API = axios.create({
+  baseURL: "http://127.0.0.1:8000/",
+});
 
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("access");
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-// export default API;
+export default API;
 
 
 // import axios from "axios";
@@ -29,32 +29,6 @@
 
 // export default API;
 
-
-
-
-
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("access");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-export default API;
-
 // import axios from 'axios';
 
 // const API = axios.create({
@@ -69,5 +43,29 @@ export default API;
 //     }
 //     return config;
 // });
+
+// export default API;
+
+
+
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://127.0.0.1:8000/",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// API.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("access");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
 
 // export default API;
